d89c983ea97b36763a996e6f6c011b99d2f75f5d
RanganadhY <ranganadhyadlapalli@outlook.com>
2023-05-21 15:20:57 +0530

    added assets

--- client/src/pages/landingPage.jsx
+++ client/src/pages/landingPage.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import "../css/landingPage.css"
+import Hero from "../assets/images/ezgif.com-video-to-gif.gif"
+import Tilt from 'react-parallax-tilt';
 
 function LandingPage() {
     return (
-        <></>
+        <>
+            <div className="main-page-wrapper">
+                    <img src={Hero} alt="" />
+            </div>
+        </>
     )
 }
 
