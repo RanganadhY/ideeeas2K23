123e2f00bfda87fe4071a9ad3ead9a67219f7b85
ANAGHA-N13 <anagha2003.13@gmail.com>
2023-05-23 17:26:03 +0530

    header changed

--- client/src/pages/landingPage.jsx
+++ client/src/pages/landingPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import "../css/landingPage.css"
-import Header from '../components/Header/Header.jsx';
+import Header from '../components/header/header.jsx';
 import Hero from "../assets/images/ezgif.com-video-to-gif.gif"
 import Tilt from 'react-parallax-tilt';
 import Cards from '../components/cards/cards';
@@ -10,34 +10,46 @@ function LandingPage() {
     return (
         <>
             <div className="main-page-wrapper">
-            <Header/>
+                <Header />
                 <div className='home-hero-section'>
-                   
-                    <div className='home-hero-title'>
-                        <div className='home-hero-title-letters'>
-                        <div className='hero-home-title-i'>
-                            I
+                    <div className='home-hero-heading-container'>
+                        <div className='home-hero-title'>
+                            <div className='home-hero-title-letters'>
+                                <div className='hero-home-title-i'>
+                                    I
+                                </div>
+
+                                <div className='hero-home-title-d'>
+                                    D
+                                </div>
+                                <div className=' hero-home-title-e'>
+                                    EEE
+                                </div>
+                                <div className='hero-home-title-as'>
+                                    AS
+                                </div>
                             </div>
-                                 D
-                             <div className=' hero-home-title-e'>
-                               EEE
+                            <div className='hero-home-title-year'>
+                                2K23
                             </div>
-                             AS 2K23
-                             </div>      
+                        </div>
+
                         <div className='home-hero-subtitle'>
                             <div className='home-hero-Otherletters'> ORGANIZED BY </div>
                             <div className='home-hero-space'> .</div>
                             <div className='home-hero-letters'>IEEE SIT SB</div>
                         </div>
                     </div>
-                    <img className='main-page-wrapper-image' src={Hero} alt="" />
+                    <div className='home-hero-image'>
+                        <img src={Hero} alt="" />
+                    </div>
                 </div>
                 <div className='home-events-title'>
                     OUR EVENTS
                 </div>
-                <div className='home-event-cards'>
+                {/* <div className='home-event-cards'>
                     <Cards details={cardData} />
-                </div>
+                </div> */}
             </div>
         </>
     )
