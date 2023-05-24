f4718dd2f5d66a7932d90eeb1032ce6bb2010a87
ANAGHA-N13 <anagha2003.13@gmail.com>
2023-05-24 11:54:21 +0530

    responsiveness added

--- client/src/pages/landingPage.jsx
+++ client/src/pages/landingPage.jsx
@@ -13,7 +13,7 @@ function LandingPage() {
                 <Header />
                 <div className='home-hero-section'>
                     <div className='home-hero-heading-container'>
-                        <div className='home-hero-title'>
+                        {/* <div className='home-hero-title'>
                             <div className='home-hero-title-letters'>
                                 <div className='hero-home-title-i'>
                                     I
@@ -32,13 +32,23 @@ function LandingPage() {
                             <div className='hero-home-title-year'>
                                 2K23
                             </div>
-                        </div>
+                        </div> */}
+                        <div className='home-hero-title-sp'>    
+                            {/* <span className='hero-home-title-i'>I</span>
+                            <span className='home-hero-title-Otherletters'>D</span>
+                            <span className='hero-home-title-i'>EEE</span>
+                            <span className='home-hero-title-Otherletters'>AS</span>
 
-                        <div className='home-hero-subtitle'>
+                            <span className='home-hero-title-Otherletters'>2K23</span> */}
+
+                            <span className='home-hero-title-letters'><span className='hero-home-title-i'>I</span>D<span className='hero-home-title-i'>EEE</span>AS  <span>2K23</span></span>
+                            <span className='home-hero-subtitle'><span className='home-hero-Otherletters'>ORGANIZED  BY</span><span>IEEE SIT SB</span></span>
+                        </div>
+                        {/* <div className='home-hero-subtitle'>
                             <div className='home-hero-Otherletters'> ORGANIZED BY </div>
                             <div className='home-hero-space'> .</div>
                             <div className='home-hero-letters'>IEEE SIT SB</div>
-                        </div>
+                        </div> */}
                     </div>
                     <div className='home-hero-image'>
                         <img src={Hero} alt="" />
