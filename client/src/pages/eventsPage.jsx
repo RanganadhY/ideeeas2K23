5805e8aeea0b65ce346866658a259a1714d03de3
ANAGHA-N13 <anagha2003.13@gmail.com>
2023-05-24 22:25:43 +0530

    events page changed

--- client/src/pages/eventsPage.jsx
+++ client/src/pages/eventsPage.jsx
@@ -2,43 +2,44 @@ import React from 'react'
 import Animation from '../components/animation/animation'
 import Tilt from 'react-parallax-tilt';
 import '../css/eventsPage.css';
-import webifyImg from '../assets/images/webify.webp'
-import Cards from '../components/cards/cards'
-import cardData from '../components/cards/cardData'
-// import {Cards,cardData} from '../components/cards/cards'
+import webifyPoster from '../assets/images/posters/webify copy 2.webp'
 function eventsPage() {
   return (
-    <>
-    <div className='eve-main-container'>
-      <Animation />
-      <div className='eve-sub-container'>
-       Events
-      </div>
-      </div>
-      <div className='eve-title'>
-       {/* OUR EVENTS  */}
-      </div>
-      {/* <div className='eve-cards'>
-      <Cards details = {cardData}/>
-      </div> */}
-      {/* <div className='events-card-main'>
-     
-        <div className='eve-card-img'>
-          <img src={webifyImg} alt='Webify-image'></img>
-          <div className='eve-card-body'>
-            <div className='eve-card-heading'>
-              web designing contest
-            </div>
-            <div className='eve-card-btn'>
-              <button>REGISTER NOW</button>
-            </div>
+    <div className='events-page-wrapper'>
+      <div className='eve-main-container'>
+        <Animation />
+        <div className='eve-sub-container'>
+          <div className='eve-main-heading'>
+            Events
           </div>
         </div>
 
-      </div>
-    */}
 
-    </>
+      </div>
+      {/* <div className='eve-title'>
+        OUR EVENTS
+      </div> */}
+      <div className='eve-content-container'>
+        <div className='eve-content-text'>
+          <div className='eve-content-text-heading'>
+            WEBIFY
+            </div>
+            <div className='eve-content-text-desc'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti amet commodi ad expedita nesciunt, tempore error reiciendis! Veritatis distinctio sint ipsum dicta neque ullam tempore corporis fuga. Eum, quaerat consequuntur.
+            </div>
+            <div className='eve-register-btn'>
+          <button>REGISTER</button>
+            </div>
+        </div>
+        <div className='eve-content-poster'>
+          <div className='eve-content-poster-image'>
+          <img src={webifyPoster} />
+
+          </div>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
