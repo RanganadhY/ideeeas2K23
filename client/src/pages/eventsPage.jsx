3e55356ea3868aedb340215d8ec3b2247bd0a58d
ANAGHA-N13 <anagha2003.13@gmail.com>
2023-05-25 07:51:34 +0530

    events page changed

--- client/src/pages/eventsPage.jsx
+++ client/src/pages/eventsPage.jsx
@@ -1,46 +1,61 @@
 import React from 'react'
+import { useParams } from 'react-router-dom';
 import Animation from '../components/animation/animation'
 import Tilt from 'react-parallax-tilt';
 import '../css/eventsPage.css';
-import webifyPoster from '../assets/images/posters/webify copy 2.webp'
-function eventsPage() {
+import Header from '../components/header/header'
+// import webifyPoster from '../assets/images/posters/webify copy 2.webp'
+import details from './events.json'
+function EventsPage(props) {
+  const { eventNum } = useParams();
+  const eventDetails = details.details[parseInt(eventNum) - 1];
+  console.log(eventNum)
+
   return (
+    <>
+    
     <div className='events-page-wrapper'>
-      <div className='eve-main-container'>
+
+      
+         <Header/>
+      {/* <div className='eve-main-container'>
         <Animation />
+     
         <div className='eve-sub-container'>
           <div className='eve-main-heading'>
-            Events
+            EVENTS
           </div>
         </div>
 
 
-      </div>
+      </div> */}
       {/* <div className='eve-title'>
         OUR EVENTS
       </div> */}
       <div className='eve-content-container'>
         <div className='eve-content-text'>
           <div className='eve-content-text-heading'>
-            WEBIFY
+            {eventDetails.name}
+            
             </div>
-            <div className='eve-content-text-desc'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti amet commodi ad expedita nesciunt, tempore error reiciendis! Veritatis distinctio sint ipsum dicta neque ullam tempore corporis fuga. Eum, quaerat consequuntur.
-            </div>
+            <span className='eve-content-text-desc'>
+              {eventDetails.description}
+            </span>
             <div className='eve-register-btn'>
           <button>REGISTER</button>
+          <button>DOWNLOAD BROCHURE</button>
             </div>
         </div>
         <div className='eve-content-poster'>
           <div className='eve-content-poster-image'>
-          <img src={webifyPoster} />
+          <img src={"/assets/images/posters/" + eventDetails.poster} />
 
           </div>
         </div>
       </div>
-
     </div>
+  </>
   )
 }
 
-export default eventsPage
+export default EventsPage
