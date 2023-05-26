8ac2c30627dcefe45242c8cf942daac0b805f08c
ANAGHA-N13 <anagha2003.13@gmail.com>
2023-05-26 20:03:20 +0530

    about routing done

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -42,7 +42,7 @@ function Header() {
       <div className='header-links'>
         <ul className={clicked ? 'header-list' : 'header-list-close'}>
           <li> <Link className='header-links' exact to="/" activeClassName="active">Home</Link> </li>
-          <li> <Link className='header-links' exact to="/about" activeClassName="active">About</Link> </li>
+          <li> <Link className='header-links' exact to="/ScrollingPage" activeClassName="active">About </Link> </li>
           <li> <Link className='header-links' exact to="/contact" activeClassName="active">Contact</Link> </li>
 
         </ul>
