5dd39c9d50f2a965a80bc557823dde8a0747fa40
ANAGHA-N13 <anagha2003.13@gmail.com>
2023-05-27 06:37:09 +0530

    Final changes made

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<LandingPage/>}/>
-        <Route path='/:eventNum' element={<EventsPage/>}/>
+        <Route path='/events/:eventNum' element={<EventsPage/>}/>
         <Route path='/ScrollingPage' element={<ScrollingPage/>}/>
         
       </Routes>
