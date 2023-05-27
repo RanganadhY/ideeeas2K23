ebe75b0a36584329b42769fab38cfe2f805b5377
Rishith P <rishithp2003@gmail.com>
2023-05-28 00:44:24 +0530

    updated the about us page

--- client/src/components/aboutus/First.js
+++ client/src/components/aboutus/First.js
@@ -1,18 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../aboutus/First.css';
 
 function First() {
+  const [currentImage, setCurrentImage] = useState(1);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImage((prevImage) => prevImage === 3 ? 1 : prevImage + 1);
+    }, 3000); 
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
-    <div className='mains'>
-      <div className='top'>
-        <h1>About Ushwfrhfkj </h1>
+    <div className='firstpage'>
+      <div className='aboutus'>
+        <h1>About Us</h1>
       </div>
-      <div className='bottom'>
-        <div className='image'></div>
-        
+      <div className='twosections'>
+        <div className='textcontent'>
+          <h1><span>IEEE </span>SIT SB</h1>
+          <p className='parieee'>IEEE SIT SB was established in the year 2002 under the inspiring leadership of Professor Basavarajaiah. The legacy was followed by the vision and direction of our mentors Dr. K V Suresh, Professor, Department of ECE and Dr. N R Sunitha, Professor, Department of CSE. Our current branch counsellors  Dr. K V Vishwanath, Professor, Department of ETE and Dr. Veena Karjagi, Associate Professor, Department of ECE  IEEE SIT SB have won a lot of laurels.
+This is the only student branch from Bangalore Section to have been published in IEEE India Council Newsletter, 2019. 
+IEEE SIT SB team is a force of diverse skills and expertise. The synergy of the team brings out the opportunity for every individual to explore, enhance and thereby strengthening the team.
+Providing insights and opportunities of IEEE, we fervently organize various events, technical colloquiums and contests.
+The WIE IEEE SIT SB, a wing under IEEE SIT SB supports all the innovation and technology of, for and by women through organizing various events.</p>
+        </div>
+        <div className='cards'>
+          <div className="gallery">
+            <div className={`gallery__item gallery__item--${currentImage}`}></div>
+          </div>
         </div>
       </div>
-    
+    </div>
   );
 }
 
--- client/src/pages/about.js
+++ client/src/pages/about.js
@@ -1,46 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../css/about.css';
 
-
-
 const About = () => {
+  const [currentImage, setCurrentImage] = useState(1);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImage((prevImage) => prevImage === 2 ? 1 : prevImage + 1);
+    }, 3000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
-    <div className='main'>
-      
-      
-      <div className="mainbox"  >
-        <div className="imagebox">
-       
+    <div className='secondpage'>
+      <div className='twosections2'>
+        <div className='textcontent2'>
+          <h1>OUR COLLEGE</h1>
+          <p className='paraieee'>Siddaganga Institute of Technology has a rich heritage of bestowing knowledge and passion to young minds. Its motto ‘work is worship’ is ingrained into the minds of every student thus creating an inclusive and empowering community for students that encourages growth and achievement. The unwavering assistance from professors and administrators played a significant role in building the confidence and perspective of every individual.</p>
         </div>
-        <div className="textbox">
-            
-            <h1 >OUR COLLEGE</h1>
-            <p className='sid'>
-            Siddaganga Institute of Technology has a rich heritage of  bestowing knowledge and passion to young minds.  Its motto ‘work is worship’ is ingrained into the minds of every student thus creating an inclusive and empowering community for students that encourages growth and achievement.
-The unwavering assistance from professors and administrators played a significant role in building the confidence and perspective of every individual.<br/>
-           </p>
+        <div className='cards2'>
+          <div className="gallery2">
+            <div className={`gallery__item2 gallery__item2--${currentImage}`}></div>
+          </div>
         </div>
-    </div>
-       <div className='secondbox'>
-       <div className="imagebox2">
-       
-            </div>
-            <div className="textbox2">
-                <h1 className='sit'> <span>IEEE</span > SIT SB</h1>
-                <p className='ieee' >
-                IEEE SIT SB was established in the year 2002 under the inspiring leadership of Professor Basavarajaiah. The legacy was followed by the vision and direction of our mentors Dr. K V Suresh, Professor, Department of ECE and Dr. N R Sunitha, Professor, Department of CSE. Our current branch counsellors  Dr. K V Vishwanath, Professor, Department of ETE and Dr. Veena Karjagi, Associate Professor, Department of ECE  IEEE SIT SB have won a lot of laurels.
-This is the only student branch from Bangalore Section to have been published in IEEE India Council Newsletter, 2019. 
-IEEE SIT SB team is a force of diverse skills and expertise. The synergy of the team brings out the opportunity for every individual to explore, enhance and thereby strengthening the team.
-Providing insights and opportunities of IEEE, we fervently organize various events, technical colloquiums and contests.
-The WIE IEEE SIT SB, a wing under IEEE SIT SB supports all the innovation and technology of, for and by women through organizing various events.<br/>
-               </p>
-            </div>
-        
-       </div>
-       
-        
       </div>
-    
+    </div>
   );
 }
 
