8202c756bfaff01ff6db7fa993816af5a634d741
ANAGHA-N13 <anagha2003.13@gmail.com>
2023-06-01 07:56:02 +0530

    links of few events changed

--- client/src/components/footer/footer.jsx
+++ client/src/components/footer/footer.jsx
@@ -4,6 +4,8 @@ import instagram from '../../assets/images/contact/instagram.webp'
 import youtube from '../../assets/images/contact/youtube.webp'
 import email from '../../assets/images/contact/email.webp'
 import blog from '../../assets/images/contact/blog.webp'
+import telegram from '../../assets/images/contact/telegram3.png'
+import { TbBrandTelegram } from 'react-icons/tb';
 
 
 export default function Footer() {
@@ -14,7 +16,9 @@ export default function Footer() {
                 <a rel="noreferrer" href="https://www.instagram.com/ieeesitsb/" target="_blank"><img alt="insta" src={instagram} className="con-icons"></img></a>
                 <a rel="noreferrer" href="https://www.youtube.com/channel/UCPatXnxDMcEDlFEQKX3q3rg" style={{"marginTop":"2vh"}} target="_blank"><img alt="youtube" src={youtube} className="con-icons"></img></a>
                 <img alt="email" src={email} className="con-icons" data-tip="ieee2021.22@gmail.com"></img>
-                <a rel="noreferrer" href="http://ieeesitstudentbranch.blogspot.com/" target="_blank"><img alt="blog" src={blog} className="con-icons"></img></a>
+                <a rel="noreferrer" href="https://ieeesit.blogspot.com/?m=1" target="_blank"><img alt="blog" src={blog} className="con-icons"></img></a>
+                <a rel="noreferrer" href="https://t.me/ieeesitsb" target="_blank"><img alt="telegram" src={telegram}className="con-icons"></img></a>
+
 
             </div>
             <center className='footer-p' style={{"color":"#b7b4b4","marginTop":"2vh"}}>All rights reserved &#169; IdEEEas, IEEE SIT SB</center>
