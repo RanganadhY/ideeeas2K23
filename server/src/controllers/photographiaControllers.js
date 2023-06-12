f04764f37704d535bbac116d290b5f616ec67e31
ANAGHA-N13 <anagha2003.13@gmail.com>
2023-06-12 12:10:28 +0530

    server added

--- server/src/controllers/photographiaControllers.js
+++ server/src/controllers/photographiaControllers.js
@@ -1,2 +1,25 @@
-const photographiaStudentSchema = require('../models/photographiaStudentModel')
+const photographiaAdminSchema = require('../models/photographiaAdminModel')
+// const { uploadFile } = require('./s3')
 
+const uploadImageDetails = async(req,res) => {
+    try{
+        const {name} = req.body;
+        const photographiaImg = req.files.photographiaImg[0];
+
+        const imageS3 = await uploadFile(photographiaImg);
+        console.log(imageS3)
+        const imageDetails = {
+            "name":{name},
+            "photo":imageS3.Location
+        }
+
+        const result = await photographiaAdminSchema.create(imageDetails);
+        return res.status(500).json({"image and details uploaded successfully":result})
+    }
+    catch(err){
+        console.log(err)
+        res.status(200).json({"message":"error in uploading image and its details"})
+    }
+}
+
+module.exports.uploadImageDetails = uploadImageDetails
